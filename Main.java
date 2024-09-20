4e3f9ef843f2ffb49d7d5b835a075848bd193b27
heta76 <lenzhaev@gmail.com>
2024-09-20 23:42:17 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package org.knit;
+
+import org.knit.lab1.Task1;
+import org.knit.lab1.SimpleUrl;
+
+public class Main {
+    public static void main(String[] args) {
+//        Task1 task1 = new Task1();
+//        task1.execute();
+
+        SimpleUrl SimpleUrl = new SimpleUrl();
+        SimpleUrl.toString();
+    }
+}

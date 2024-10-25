9e569282e0e7a1045cf494340cc4599c798860e6
heta76 <lenzhaev@mail.ru>
2024-10-25 20:41:13 +0400

    lab 5 task 8

--- src/main/java/org/knit/lab5/Main.java
+++ src/main/java/org/knit/lab5/Main.java
@@ -1,24 +1,34 @@
 package org.knit.lab5;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Random;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         String[] names = {"книга", "ручка", "линейка", "пенал"};
         Random random = new Random();
-        Comparator<ShopItem> comparator = Comparator.comparing(ShopItem::getPrice).thenComparing(ShopItem::getArticule);
+        Comparator<ShopItem> comparator = Comparator.comparing(ShopItem::getName).thenComparing(ShopItem::getArticule);
         ShopItem[] items = new ShopItem[100];
-        int count = 0;
+        HashMap<String, Integer> itemsCount = new HashMap<>();
+
         for (int i = 0; i < 100; i++) {
+
             int index = random.nextInt(4);
             items[i] = new ShopItem(names[index]);
         }
-        for (int i = 0; i < items.length - 1; i++) {
-            for (int j = 1; j < items.length; j++){
-                if (items[i].equals(items[j])){
-                    count+=1;
+        /*
+        Во внешнем цикле, если есть товар, то скипается и товар, и внутренний цикл.
+        Если товара нет, то добавляется и проходит по внутреннему циклу.
+         */
+        for (int i = 0; i < items.length; i++) {
+            if (itemsCount.containsKey(items[i].getName() + items[i].getArticule().toString())){
+                continue;
+            }
+            itemsCount.put(items[i].getName() + items[i].getArticule().toString(), 1);
+            for (int j = i + 1; j < items.length; j++) {
+                if (items[i].equals(items[j])) {
+                    System.out.println("боба");
+                    itemsCount.put(items[i].getName() + items[i].getArticule().toString(), itemsCount.get(items[i].getName() + items[i].getArticule().toString()) + 1);
                 }
             }
         }
@@ -26,6 +36,11 @@ public class Main {
         for (ShopItem product : items) {
             System.out.println(product);
         }
-        System.out.println(count);
+        for (Map.Entry<String, Integer> entry : itemsCount.entrySet()) {
+            if (entry.getValue() > 1){
+                System.out.println(entry);
+            }
+        }
     }
 }
+
--- src/main/java/org/knit/lab5/ShopItem.java
+++ src/main/java/org/knit/lab5/ShopItem.java
@@ -8,20 +8,21 @@ public class ShopItem {
     private int price;
     private String name;
     private int articule;
-    private int shop = 4;
+    private int shop;
     Random random = new Random();
 
 
     public ShopItem(String name) {
         this.name = name;
-        this.price = random.nextInt(10);
-        this.articule = random.nextInt(20);
+        this.price = random.nextInt(10) + 5;
+        this.articule = random.nextInt(20) + 1;
+        this.shop = random.nextInt(5) + 1;
     }
 
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (obj == null || !obj.getClass().equals(this.getClass())) return false;
         ShopItem shopItem = (ShopItem) obj;  // Приведение типа
         return articule == shopItem.articule && name.equals(shopItem.name);
     }
@@ -33,8 +34,8 @@ public class ShopItem {
 
     @Override
     public String toString() {
-        return name  + " цена: " + price  + " № товара: " + articule +
-                " № магазина: " + shop;
+        return name   + " № товара: " + articule + " цена: " + price
+        + " № магазина: " + shop;
     }
 
     public Integer getArticule() {
@@ -43,5 +44,9 @@ public class ShopItem {
 
     public Integer getPrice() {
         return price;
+    }
+
+    public String getName() {
+        return name;
     }
 }
